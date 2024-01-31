354c32015f22c41a2245d6dddb9b621452e963f8
wonsoong <mike9159@naver.com>
2024-01-31 14:16:06 +0900

    k8s-web-to-nginx

---
+++ k8s-web-to-nginx/index.mjs
@@ -0,0 +1,22 @@
+import express from 'express'
+import os from 'os'
+
+const app = express()
+const PORT = 3000
+
+app.get("/", (req, res) => {
+  const helloMessage = `안녕 Ver.2 ${os.hostname()}`
+  console.log(helloMessage)
+  res.send(helloMessage)
+})
+
+app.get("/nginx", async(req, res) => {
+  const url = 'http://nginx'
+  const response = await fetch(url);
+  const body = await response.text();
+  res.send(body)
+})
+
+app.listen(PORT, () => {
+  console.log(`Web server is listening at port ${PORT}`)
+})
